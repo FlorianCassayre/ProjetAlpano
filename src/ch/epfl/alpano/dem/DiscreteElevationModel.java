e9a4faa4eae44642304a602e5d9ffef340c41235
Florian Cassayre <florian.cassayre@gmail.com>
2017-02-27 14:09:51 +0100

    Completed DiscreteElevationModel.

--- src/ch/epfl/alpano/dem/DiscreteElevationModel.java
+++ src/ch/epfl/alpano/dem/DiscreteElevationModel.java
@@ -19,17 +19,39 @@ public interface DiscreteElevationModel extends AutoCloseable
      */
     double SAMPLES_PER_RADIAN = 360 * SAMPLES_PER_DEGREE / Math2.PI2;
 
+    /**
+     * Returns the sample index at the given coordinate. The angle can be a longitude or a latitude, expressed in radians.
+     * @param angle an angle in radians
+     * @return the sample index
+     */
     static double sampleIndex(double angle)
     {
-        throw new NotImplementedException();
+        return angle * SAMPLES_PER_RADIAN;
     }
 
+    /**
+     * Returns the DTM extent.
+     * @return a bi-dimensional interval
+     */
     Interval2D extent();
 
+    /**
+     * Return the altitude sample at the given coordinates.
+     * Throws {@link IllegalArgumentException} if the index if out of the DTM bounds.
+     * @param x the x coordinate
+     * @param y the y coordinate
+     * @return the elevation
+     */
     double elevationSample(int x, int y);
 
+    /**
+     * Returns the union of the two models.
+     * Throws {@link IllegalArgumentException} if the models are not unionable.
+     * @param that the other DTM
+     * @return an instance of {@link CompositeDiscreteElevationModel}
+     */
     default DiscreteElevationModel union(DiscreteElevationModel that)
     {
-        throw new NotImplementedException();
+        return new CompositeDiscreteElevationModel(this, that);
     }
 }
