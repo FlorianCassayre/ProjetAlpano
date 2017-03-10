3968bfab4b43a7ad2435dd2c4549acf595362146
Florian Cassayre <florian.cassayre@gmail.com>
2017-03-10 14:24:30 +0100

    Started the implementation of ElevationProfile.

--- src/ch/epfl/alpano/dem/ElevationProfile.java
+++ src/ch/epfl/alpano/dem/ElevationProfile.java
@@ -1,14 +1,35 @@
 package ch.epfl.alpano.dem;
 
-import ch.epfl.alpano.GeoPoint;
+import ch.epfl.alpano.*;
+
+import java.util.Objects;
 
 public final class ElevationProfile
 {
+    private final ContinuousElevationModel elevationModel;
+    private final GeoPoint origin;
+    private final double azimuth, length;
+
     public ElevationProfile(ContinuousElevationModel elevationModel, GeoPoint origin, double azimuth, double length)
     {
-        // TODO: IllegalArgumentException
-        // TODO: NullPointerException
-        throw new UnsupportedOperationException();
+        this.elevationModel = Objects.requireNonNull(elevationModel);
+        this.origin = Objects.requireNonNull(origin);
+
+        Preconditions.checkArgument(Azimuth.isCanonical(azimuth));
+        this.azimuth = azimuth;
+
+        Preconditions.checkArgument(length > 0);
+        this.length = length;
+    }
+
+    /**
+     * Checks if the distance in in the bounds of the profile
+     * @param x
+     * @return
+     */
+    private boolean isInBounds(double x)
+    {
+        return x >= 0 && x <= length;
     }
 
     public double elevationAt(double x)
