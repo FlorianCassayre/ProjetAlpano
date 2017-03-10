5cb4f15deb129ccc6ffe0a83691f189be659768f
moussaprinceepfl <moussa.prince@epfl.ch>
2017-03-10 14:46:14 +0100

    Elevation Profile continued

--- src/ch/epfl/alpano/dem/ElevationProfile.java
+++ src/ch/epfl/alpano/dem/ElevationProfile.java
@@ -10,6 +10,13 @@ public final class ElevationProfile
     private final GeoPoint origin;
     private final double azimuth, length;
 
+    /**
+     * Creates an elevation profile following a circle arc
+     * @param elevationModel the continuous MNT
+     * @param origin the origin
+     * @param azimuth the direction
+     * @param length the length of the profile
+     */
     public ElevationProfile(ContinuousElevationModel elevationModel, GeoPoint origin, double azimuth, double length)
     {
         this.elevationModel = Objects.requireNonNull(elevationModel);
@@ -23,30 +30,38 @@ public final class ElevationProfile
     }
 
     /**
-     * Checks if the distance in in the bounds of the profile
-     * @param x
-     * @return
+     * Checks if the position is in the bounds of the profile
+     * @param x the position to test
+     * @return true if the position is in the bounds, false else
+     * @throws IllegalArgumentException if the position is not in the bounds
      */
     private boolean isInBounds(double x)
     {
         return x >= 0 && x <= length;
     }
 
+    /**
+     * Gives the altitude of the field at a given position
+     * @param x the position given
+     * @return the altitude
+     * @throws IllegalArgumentException if position is not in the bounds
+     */
     public double elevationAt(double x)
     {
-        // TODO: IllegalArgumentException
+        isInBounds(x);
         throw new UnsupportedOperationException();
     }
 
     public GeoPoint positionAt(double x)
     {
-        // TODO: IllegalArgumentException
+        isInBounds(x);
+
         throw new UnsupportedOperationException();
     }
 
     public double slopeAt(double x)
     {
-        // TODO: IllegalArgumentException
+        isInBounds(x);
         throw new UnsupportedOperationException();
     }
 }
