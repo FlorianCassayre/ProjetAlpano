0a92dba69f9688327b3b24efe13a2ccc35bcaaaa
moussaprinceepfl <moussa.prince@epfl.ch>
2017-03-17 14:55:20 +0100

    PanoramaParameters Test

---
+++ test/ch/epfl/alpano/PanoramaParametersTest.java
@@ -0,0 +1,14 @@
+package ch.epfl.alpano;
+
+import org.junit.Test;
+
+public class PanoramaParametersTest
+{
+    @Test
+    public void testAzimuthForX()
+    {
+        PanoramaParameters p = new PanoramaParameters(new GeoPoint(6.8087, 47.0085), 1380, Math.toRadians(162), Math.toRadians(27), 300, 2500, 800);
+//TODO
+    }
+
+}
