14835833295ce350b0384c861bcf533b736b7102
Florian Cassayre <florian.cassayre@gmail.com>
2017-03-17 12:58:23 +0100

    Added some tests.

--- test/ch/epfl/alpano/dem/ElevationProfileTest.java
+++ test/ch/epfl/alpano/dem/ElevationProfileTest.java
@@ -28,6 +28,29 @@ public class ElevationProfileTest
         }
     }
 
+    private ElevationProfile createElevationProfile()
+    {
+        return new ElevationProfile(new ContinuousElevationModel(new HgtDiscreteElevationModel(new File("res/data/N46E006.hgt"))), new GeoPoint(Math.toRadians(6), Math.toRadians(46)), Math.toRadians(45), 102400);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testIllegalConstructorValues1()
+    {
+        new ElevationProfile(new ContinuousElevationModel(new HgtDiscreteElevationModel(new File("res/data/N46E006.hgt"))), new GeoPoint(Math.toRadians(6), Math.toRadians(46)), Math.toRadians(45), 0);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testIllegalConstructorValues2()
+    {
+        new ElevationProfile(new ContinuousElevationModel(new HgtDiscreteElevationModel(new File("res/data/N46E006.hgt"))), null, Math.toRadians(45), 102400);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testIllegalConstructorValues3()
+    {
+        new ElevationProfile(new ContinuousElevationModel(new HgtDiscreteElevationModel(null)), new GeoPoint(Math.toRadians(6), Math.toRadians(46)), Math.toRadians(45), 102400);
+    }
+
     @Test
     public void testCompareElevation() throws IOException
     {
@@ -40,7 +63,7 @@ public class ElevationProfileTest
     @Test
     public void testPositionAtWithExactValue()
     {
-        final ElevationProfile profile = new ElevationProfile(new ContinuousElevationModel(new HgtDiscreteElevationModel(new File("res/data/N46E006.hgt"))), new GeoPoint(Math.toRadians(6), Math.toRadians(46)), Math.toRadians(45), 100000);
+        final ElevationProfile profile = createElevationProfile();
         final double expectedLongitude = Math.toRadians(6.09385);
         final double expectedLatitude = Math.toRadians(46.06508);
 
@@ -57,7 +80,7 @@ public class ElevationProfileTest
         final double[] expectedLongitudes = new double[] {6.00000, 6.03751, 6.07506, 6.11265, 6.94857};
         final double[] expectedLatitudes = new double[] {46.00000, 46.02604, 46.05207, 46.07809, 46.64729};
 
-        final ElevationProfile profile = new ElevationProfile(new ContinuousElevationModel(new HgtDiscreteElevationModel(new File("res/data/N46E006.hgt"))), new GeoPoint(Math.toRadians(6), Math.toRadians(46)), Math.toRadians(45), 102400);
+        final ElevationProfile profile = createElevationProfile();
         for(int i = 0; i < distances.length; i++)
         {
             final double distance = distances[i];
@@ -69,6 +92,59 @@ public class ElevationProfileTest
             assertEquals(expectedLongitude, actual.longitude(), 1.0 / DiscreteElevationModel.SAMPLES_PER_RADIAN);
             assertEquals(expectedLatitude, actual.latitude(), 1.0 / DiscreteElevationModel.SAMPLES_PER_RADIAN);
         }
+    }
 
+    @Test
+    public void testElevationAtForIllegalValues()
+    {
+        final ElevationProfile profile = createElevationProfile();
+        final double[] values = {-1, Double.NaN, Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY, 102401};
+
+        for(double value : values)
+        {
+            try
+            {
+                profile.elevationAt(value);
+                fail();
+            }
+            catch(IllegalArgumentException ex)
+            {}
+        }
+    }
+
+    @Test
+    public void testPositionAtForIllegalValues()
+    {
+        final ElevationProfile profile = createElevationProfile();
+        final double[] values = {-1, Double.NaN, Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY, 102401};
+
+        for(double value : values)
+        {
+            try
+            {
+                profile.positionAt(value);
+                fail();
+            }
+            catch(IllegalArgumentException ex)
+            {}
+        }
+    }
+
+    @Test
+    public void testSlopeAtForIllegalValues()
+    {
+        final ElevationProfile profile = createElevationProfile();
+        final double[] values = {-1, Double.NaN, Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY, 102401};
+
+        for(double value : values)
+        {
+            try
+            {
+                profile.slopeAt(value);
+                fail();
+            }
+            catch(IllegalArgumentException ex)
+            {}
+        }
     }
 }
