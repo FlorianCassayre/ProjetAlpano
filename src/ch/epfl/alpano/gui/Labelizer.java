d76946a3fad26f9516ccf4432b2fa897106bf551
Florian Cassayre <florian.cassayre@gmail.com>
2017-04-26 14:02:02 +0200

    WIP.

--- src/ch/epfl/alpano/gui/Labelizer.java
+++ src/ch/epfl/alpano/gui/Labelizer.java
@@ -4,6 +4,10 @@ import ch.epfl.alpano.PanoramaParameters;
 import ch.epfl.alpano.dem.ContinuousElevationModel;
 import ch.epfl.alpano.summit.Summit;
 import javafx.scene.Node;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -23,11 +27,39 @@ public final class Labelizer
 
     public List<Node> labels(PanoramaParameters parameters)
     {
-        throw new UnsupportedOperationException();
+        final List<Node> nodes = new ArrayList<>();
+
+        final int yMax = 170;
+
+        for(Summit summit : getVisibleSummits(parameters))
+        {
+            
+
+            final double azimuth = 0.0; // TODO
+            final double verticalAngle = 0.0; // TODO
+
+            final double x = parameters.xForAzimuth(azimuth), y = parameters.yForAltitude(verticalAngle);
+
+            Text text = new Text(summit.name());
+            text.getTransforms().addAll(new Translate(x, yMax), new Rotate(45, 0, 0));
+            nodes.add(text);
+
+            Line line = new Line(x, y, x, yMax);
+            nodes.add(line);
+        }
+
+        return nodes;
     }
 
-    private List<Summit> getVisibleSummits()
+    private List<Summit> getVisibleSummits(PanoramaParameters parameters)
     {
-        throw new UnsupportedOperationException();
+        final List<Summit> visible = new ArrayList<>();
+
+        for(Summit summit : summits)
+        {
+
+        }
+
+        return visible;
     }
 }
