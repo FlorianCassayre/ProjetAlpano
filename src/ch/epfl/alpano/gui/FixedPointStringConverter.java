17ebd9a82012745435f8cfc5fce752beee3f68ea
Florian Cassayre <florian.cassayre@gmail.com>
2017-05-01 13:56:11 +0200

    Simplifications for FixedPointStringConverter.

--- src/ch/epfl/alpano/gui/FixedPointStringConverter.java
+++ src/ch/epfl/alpano/gui/FixedPointStringConverter.java
@@ -18,26 +18,12 @@ public final class FixedPointStringConverter extends StringConverter<Integer>
     @Override
     public String toString(Integer n)
     {
-        BigDecimal decimal = new BigDecimal(n);
-
-        if(i > 0)
-            decimal = decimal.movePointLeft(i);
-        else if(i < 0)
-            decimal = decimal.movePointRight(-i);
-
-        return decimal.toPlainString();
+        return new BigDecimal(n).movePointLeft(i).toPlainString();
     }
 
     @Override
     public Integer fromString(String string)
     {
-        BigDecimal decimal = new BigDecimal(string);
-
-        if(i > 0)
-            decimal = decimal.movePointRight(i);
-        else if(i < 0)
-            decimal = decimal.movePointLeft(-i);
-
-        return decimal.setScale(0, RoundingMode.HALF_UP).intValueExact();
+        return new BigDecimal(string).movePointRight(i).setScale(0, RoundingMode.HALF_UP).intValueExact();
     }
 }
