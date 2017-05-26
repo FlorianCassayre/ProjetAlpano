f2cc803d5da15631bca1a50f6265393cec68b7cc
Florian Cassayre <florian.cassayre@gmail.com>
2017-05-26 15:27:41 +0200

    Corrected the canvas getting cropped.

--- src/ch/epfl/alpano/gui/MiniMapRenderer.java
+++ src/ch/epfl/alpano/gui/MiniMapRenderer.java
@@ -4,24 +4,18 @@ import ch.epfl.alpano.GeoPoint;
 import ch.epfl.alpano.Math2;
 import ch.epfl.alpano.PanoramaParameters;
 import ch.epfl.alpano.dem.ContinuousElevationModel;
-import ch.epfl.alpano.dem.ElevationProfile;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.*;
 import javafx.scene.shape.ArcType;
 import javafx.scene.transform.Rotate;
-import javafx.scene.transform.Transform;
 import javafx.scene.transform.Translate;
-
-import java.awt.image.BufferedImage;
-
-import static java.awt.image.BufferedImage.TYPE_INT_RGB;
 
 
 public interface MiniMapRenderer
 {
     int DIAMETER = 200;
-    int CENTER_X = 20, CENTER_Y = 20;
+    int MARGIN_X = 20, MARGIN_Y = 20;
     double MIN_ELEVATION = 200;
     double MAX_ELEVATION = 4000;
     double WIDTH = Math.toRadians(1);
@@ -32,7 +26,7 @@ public interface MiniMapRenderer
         GraphicsContext g = canvas.getGraphicsContext2D();
 
         g.setFill(Color.WHITE);
-        g.fillOval(0, 0, DIAMETER, DIAMETER);
+        g.fillOval(MARGIN_X, MARGIN_Y, DIAMETER, DIAMETER);
 
         final int radius = DIAMETER >> 1;
         final int distance = radius * radius;
@@ -53,18 +47,18 @@ public interface MiniMapRenderer
                     final GeoPoint point = new GeoPoint(lon, latitude);
                     final double elevation = (cdem.elevationAt(point) - MIN_ELEVATION) / (MAX_ELEVATION - MIN_ELEVATION);
                     g.setFill(Color.gray(Math.max(Math.min(elevation, 1.0), 0)));
-                    g.fillRect(x, DIAMETER - 1 - y, 1, 1);
+                    g.fillRect(x + MARGIN_X, DIAMETER - 1 - y + MARGIN_Y, 1, 1);
                 }
             }
         }
 
         g.setFill(Color.rgb(255, 0, 0, 0.5));
-        g.fillArc(0, 0, DIAMETER, DIAMETER, Math.toDegrees(-parameters.centerAzimuth() + (Math.PI - parameters.horizontalFieldOfView()) / 2.0), Math.toDegrees(parameters.horizontalFieldOfView()), ArcType.ROUND);
+        g.fillArc(MARGIN_X, MARGIN_Y, DIAMETER, DIAMETER, Math.toDegrees(-parameters.centerAzimuth() + (Math.PI - parameters.horizontalFieldOfView()) / 2.0), Math.toDegrees(parameters.horizontalFieldOfView()), ArcType.ROUND);
 
         g.setStroke(Color.BLACK);
-        g.setLineWidth(1.0);
-        g.strokeOval(0, 0, DIAMETER, DIAMETER);
-        canvas.getTransforms().addAll(new Translate(CENTER_X, CENTER_Y), new Rotate(Math.toDegrees(-parameters.centerAzimuth()), DIAMETER >> 1, DIAMETER >> 1));
+        g.setLineWidth(4.0);
+        g.strokeOval(MARGIN_X, MARGIN_Y, DIAMETER, DIAMETER);
+        canvas.getTransforms().addAll(new Rotate(Math.toDegrees(-parameters.centerAzimuth()), MARGIN_X + (DIAMETER >> 1), MARGIN_Y + (DIAMETER >> 1)));
 
         return canvas;
     }
